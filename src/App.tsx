1005c5f58671c90a2ff621cd5503662edf4581b9
Ch3k0 <sergioarieljuarez@gmail.com>
2024-01-28 18:36:49 -0300

    push simple site resume sergio mpw

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo192.png';
 import './App.css';
 
 function App() {
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {"Proximamente..."}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
