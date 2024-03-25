730a1522ff8da4325631ac2c05490e93d97e177c
Ch3k0 <sergioarieljuarez@gmail.com>
2024-03-25 07:51:00 +0100

    react-router-dom fix links, deployed

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
                 <Route path={"about/contact"} element={ <Contact/> } />
                 <Route path={"about/home"} element={ <Home/> } />
                 <Route path={"about/about"} element={ <About/> } />
+                <Route path={"about"} element={ <Home/> } />
             </Routes>
         </div>
     );
