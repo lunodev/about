6412f214b920c481e1a4773146d2e39151b51c5f
Ch3k0 <sergioarieljuarez@gmail.com>
2024-03-31 18:32:17 +0200

    add redirection for others urls non used v4, deployed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Routes, Route, Navigate } from "react-router-dom"
+import {Routes, Route, Navigate, BrowserRouter} from "react-router-dom"
 import Home from "./components/pages/Home/Home";
 import About from "./components/pages/About/About";
 import Contact from "./components/pages/Contact/Contact";
@@ -15,17 +15,17 @@ import Blog from './components/pages/Blog/Blog';
 function App() {
     return (
         <div className="App">
-            <Routes>
-                <Route path={"about/blog"} element={ <Blog data={JsonData}/> } />
-                <Route path={"about/contact"} element={ <Contact data={JsonData}/> } />
-                <Route path={"about/home"} element={ <Home data={JsonData}/> } />
-                <Route path={"about/about"} element={ <About data={JsonData}/> } />
-                <Route path={"about"} element={ <Home data={JsonData}/> } />
-                <Route
-                    path={"about/*"}
-                    element={<Navigate to={"/about"} replace/>}
-                />
-            </Routes>
+                <Routes>
+                    <Route path={"about/blog"} element={ <Blog data={JsonData}/> } />
+                    <Route path={"about/contact"} element={ <Contact data={JsonData}/> } />
+                    <Route path={"about/home"} element={ <Home data={JsonData}/> } />
+                    <Route path={"about/about"} element={ <About data={JsonData}/> } />
+                    <Route path={"about"} element={ <Home data={JsonData}/> } />
+                    <Route
+                        path={"about/*"}
+                        element={<Navigate to={"/about"} replace/>}
+                    />
+                </Routes>
         </div>
     );
 }
